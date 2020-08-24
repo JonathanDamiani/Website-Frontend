dcacdfa2ede50b12ed0b7e0ede16541f8c54042d
Jonathan Damiani <jdeandamianiapps@gmail.com>
2020-08-24 13:14:38 -0700

    Configure next for strapi using apollo
    Configutarion of next to use strapi with apollo and graphq

---
+++ apollo/queries/articleCategories.js
@@ -0,0 +1,12 @@
+import gql from "graphql-tag";
+
+const ARTICLE_CATEGORIES_QUERY = gql`
+    query articleCategories {
+        articleCategories {
+        id
+        name
+        }
+    }
+`;
+
+export default ARTICLE_CATEGORIES_QUERY;
